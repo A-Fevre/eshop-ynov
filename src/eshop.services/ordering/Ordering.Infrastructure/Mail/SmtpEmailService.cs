2317c832439cc5beb9f60d979ed8359f844e1000
malXcom <malcom.nouali@ynov.com>
2026-02-08 19:41:09 +0100

    ESHOP-021(feat):use of mailtrap to send emails

--- src/eshop.services/ordering/Ordering.Infrastructure/Mail/SmtpEmailService.cs
+++ src/eshop.services/ordering/Ordering.Infrastructure/Mail/SmtpEmailService.cs
@@ -1,12 +1,37 @@
+using System.Net;
+using System.Net.Mail;
+using Microsoft.Extensions.Configuration;
 using Ordering.Domain.Abstractions;
 
-namespace Ordering.Infrastructure.Mail;
-
-public class SmtpEmailService : IEmailService
+namespace Ordering.Infrastructure.Mail
 {
-    public async Task SendEmailAsync(string to, string subject, string body)
+    public class SmtpEmailService : IEmailService
     {
-        Console.WriteLine($"[EMAIL SENT] To: {to}, Subject: {subject}, Body: {body}");
-        await Task.CompletedTask;
+        public async Task SendEmailAsync(string to, string subject, string body)
+        {
+            var host = "sandbox.smtp.mailtrap.io";
+            var port = 2525;
+            var username = "f37b7a584faf1b";
+            var password = "ead1941478a6a7";
+
+            var client = new SmtpClient(host, port)
+            {
+                Credentials = new NetworkCredential(username, password),
+                EnableSsl = true
+            };
+
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress("mailtrap@example.com", "Ordering System"),
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true,
+            };
+            
+            mailMessage.To.Add(to);
+
+            await client.SendMailAsync(mailMessage);
+            Console.WriteLine($"[Mailtrap SMTP] Email sent to {to}");
+        }
     }
 }
