1ef5f5b588d1b8655b2e1cd4e67d8671f1fae14d
Adrien Fevre <adrien.fevre.pro@gmail.com>
2026-02-09 16:07:23 +0100

    feat(order): add ProductName column to OrderItems table

---
+++ src/eshop.services/ordering/Ordering.Infrastructure/Data/Migrations/20260209131703_UpdateData_for_name_item.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ordering.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateData_for_name_item : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProductName",
+                table: "OrderItems",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductName",
+                table: "OrderItems");
+        }
+    }
+}
